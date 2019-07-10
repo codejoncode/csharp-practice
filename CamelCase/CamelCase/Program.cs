da92f65a0af388a25046b8c1edffdf30d6f4a65e
Jonathan <jonathanjholloway@gmail.com>
2019-07-09 19:20:48 -0500

    completed CamelCase problem starting new challenge

---
+++ CamelCase/CamelCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             https://www.hackerrank.com/challenges/camelcase/problem
+
+             */
+            CamelCase("saveChangesInTheEditor");
+        }
+        //passes all tests 
+        static int CamelCase(string s)
+        {
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int count = 0; 
+            foreach(char character in s)
+            {
+                if(upperCase.Contains(character))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count + 1);
+            return count + 1; 
+        }
+    }
+}
---
+++ Strong Password/Strong Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strong_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int minimumNumber(int n, string password)
+        {
+            // Return the minimum number of characters to make the password strong
+
+        }
+    }
+}
