ae820db25aebb1fee20537aa26229183436e3f55
Jonathan <jonathanjholloway@gmail.com>
2019-07-21 16:32:58 -0500

    first practice with windows deskp application

---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/AnotherClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_2___Program_4
+{
+    class AnotherClass
+    {
+        public static void Main ()
+        {
+            MessageBox.Show("Pow!");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Chapter_2___Program_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.changeText = new System.Windows.Forms.Button();
+            this.enableCheckbox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            this.changeText.AccessibleName = "changeText";
+            this.changeText.Location = new System.Drawing.Point(12, 21);
+            this.changeText.Name = "changeText";
+            this.changeText.Size = new System.Drawing.Size(75, 23);
+            this.changeText.TabIndex = 0;
+            this.changeText.Text = "Change the label if cheked";
+            this.changeText.UseVisualStyleBackColor = true;
+            this.changeText.Click += new System.EventHandler(this.ChangeText_Click);
+            // 
+            // enableCheckbox
+            // 
+            this.enableCheckbox.AccessibleName = "enableCheckbox";
+            this.enableCheckbox.AutoSize = true;
+            this.enableCheckbox.Checked = true;
+            this.enableCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckbox.Location = new System.Drawing.Point(355, 27);
+            this.enableCheckbox.Name = "enableCheckbox";
+            this.enableCheckbox.Size = new System.Drawing.Size(131, 17);
+            this.enableCheckbox.TabIndex = 1;
+            this.enableCheckbox.Text = "Enable label changing";
+            this.enableCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.Location = new System.Drawing.Point(12, 69);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(460, 13);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Press the button to change my text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 91);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckbox);
+            this.Controls.Add(this.changeText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button changeText;
+        private System.Windows.Forms.CheckBox enableCheckbox;
+        private System.Windows.Forms.Label labelToChange;
+    }
+}
+
