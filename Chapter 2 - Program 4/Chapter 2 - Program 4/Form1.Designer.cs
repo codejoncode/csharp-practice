4e88b914d39f0381b3b6303b6b35935e250755f9
Jonathan <jonathanjholloway@gmail.com>
2019-07-21 16:42:29 -0500

    adding

--- Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.Designer.cs
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.Designer.cs
@@ -28,9 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.changeText = new System.Windows.Forms.Button();
             this.enableCheckbox = new System.Windows.Forms.CheckBox();
             this.labelToChange = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // changeText
@@ -65,16 +68,27 @@
             this.labelToChange.TabIndex = 2;
             this.labelToChange.Text = "Press the button to change my text";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(160, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 91);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelToChange);
             this.Controls.Add(this.enableCheckbox);
             this.Controls.Add(this.changeText);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +99,7 @@
         private System.Windows.Forms.Button changeText;
         private System.Windows.Forms.CheckBox enableCheckbox;
         private System.Windows.Forms.Label labelToChange;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
