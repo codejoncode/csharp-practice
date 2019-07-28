bb9b4d54432a33886b7b7133fe123f25a2469cdf
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 18:57:22 -0500

    created an Address class so that I could follow separation of responsibilities principle

---
+++ ACM/ACM.BL/Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Address
+    {
+        public Address()
+        {
+
+        }
+
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+        public string AddressType { get; set; }
+
+        ///<summary>
+        ///Validates the address
+        /// </summary>
+        /// <returns>bool</returns>
+        /// 
+        public bool Validate ()
+        {
+            //code for validation  
+            // which parts are required for the user to have updated ? 
+            return true; 
+        }
+
+    }
+}
