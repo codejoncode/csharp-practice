91940d9e8a22f86ea63b4a088c58a80ccdc84454
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 14:26:34 -0500

    added the Product class and the test file but haven't implemented test yet

---
+++ ACM/ACM.BL/Product.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Product
+    {
+        // constructor 
+        public Product()
+        {
+
+        }
+        //overloaded constructor 
+        public Product(int productId)
+        {
+            ProductId = productId; 
+        }
+        // the ? denotes a nullable type  integer or decimal can be set or  not set  
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; private set; }
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Retrieve one product. 
+        /// </summary>
+        /// <returns>Product</returns>
+        public Product Retrieve(int customerId)
+        {
+            //Code that retrieves the defined product
+
+            return new Product();
+        }
+        /// <summary>
+        /// Saves the current product.
+        /// </summary>
+        /// <returns>bool</returns
+        public bool  Save()
+        {
+            // Code that saves the defined product 
+
+            return true; 
+        }
+        /// <summary>
+        /// Validates the product data
+        /// </summary>
+        /// <returns>bool</returns>
+        /// 
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid; 
+        }
+    }
+}
---
+++ ACM/Tests/ACM.BLTest/ProductTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class ProductTest
+    { 
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
