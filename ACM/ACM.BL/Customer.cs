2e520754f1efce91868f91c003bd3272e67dc1b5
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 12:45:53 -0500

    completed the basic information needed for the Customer class

--- ACM/ACM.BL/Customer.cs
+++ ACM/ACM.BL/Customer.cs
@@ -13,11 +13,21 @@ namespace ACM.BL
         /// </summary>
         //short syntax  behind the scene the code is  creating the backing field. 
         /*if there is no need for logic in the getter or setter*/
-        public string Firstname { get; set; }
+        public string FirstName { get; set; }
         public string EmailAddress { get; set; }
         // prop tab tab  
         // any class can get the CustomerId but only this class can set it. 
         public int CustomerId { get; private set; }//propg
+      
+        //no backing feild or setter no other code would change this value and we are using variables that already have backing fields
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+
+            }
+        }
 
         //this variable is the backing field
         private string _lastName; // private access modifier  
