755f7b1d1ca2f0bbd84e6b3543cf7e0092fbc6aa
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 12:55:15 -0500

    completed the first unit test on the FullName making sure it is valid

---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullnameTestValid()
+        {
+            // -- Arrange 
+            //Customer customer = new Customer();
+            //customer.FirstName = "Bilbo";
+            //customer.LastName = "Baggins";
+            //the below is same as above. 
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+            //-- Act 
+            string actual = customer.FullName;
+            //-- Assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
