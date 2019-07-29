16d8041dba731e1bc544f2494a36dcf1d7f99f9a
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 19:30:57 -0500

    added the CustomerRepoisitory

--- ACM/ACM.BL/CustomerRepoisitory.cs
+++ ACM/ACM.BL/CustomerRepoisitory.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    class CustomerRepoisitory
+    public class CustomerRepoisitory
     {
+        /// <summary>
+        /// Retrieve one customer
+        /// </summary>
+        /// <returns>Customer</returns>
+        /// 
+        public Customer Retrieve(int customerId)
+        {
+            //Create the instance of the Customer class 
+            // Pass in the requested id 
+            Customer customer = new Customer(customerId);
+
+            // Code that retrieves the defined customer 
+
+            //Temporary hard-coded values to return 
+            //a populated customer 
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+            }
+            return customer; 
+        }
+        /// <summary>
+        /// Saves the current customer. 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        public bool Save(Customer customer)
+        {
+            // Code that saves the passed in customer 
+
+            return true; 
+        }
     }
 }
