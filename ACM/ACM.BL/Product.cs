8c9a3d269cf68df7f4d43fea33f2ab724f601b79
Jonathan <jonathanjholloway@gmail.com>
2019-07-29 18:26:57 -0500

    built out the StringHandler  class with the InsertSpaces method  for the common library

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -22,7 +22,22 @@ namespace ACM.BL
         public decimal? CurrentPrice { get; set; }
         public string ProductDescription { get; set; }
         public int ProductId { get; private set; }
-        public string ProductName { get; set; }
+
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName;
+            }
+            set
+            {
+                _productName = value; 
+            }
+        }
+
+        
+
 
         public override string ToString() => ProductName;
 
