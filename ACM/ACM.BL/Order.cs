e92143555036cfa6225d027a9c2c87367d2a77ba
Jonathan <jonathanjholloway@gmail.com>
2019-07-29 12:47:19 -0500

    added the OrderRepository and made changes to the Order class

--- ACM/ACM.BL/Order.cs
+++ ACM/ACM.BL/Order.cs
@@ -22,29 +22,6 @@ namespace ACM.BL
         public int OrderId { get; private set; }
 
         /// <summary>
-        /// Retrieve one order
-        /// </summary>
-        /// 
-        public Order Retrieve(int orderId)
-        {
-            //Code that retrieves the defined order 
-
-            return new Order(); 
-        }
-
-        /// <summary>
-        /// Saves the current order
-        /// </summary>
-        /// <returns>bool</returns>
-        /// 
-        public bool Save()
-        {
-            // Code that saves thed defined order 
-
-            return true; 
-        }
-
-        /// <summary>
         /// Validates the order data. 
         /// </summary>
         /// <returns>bool</returns>
