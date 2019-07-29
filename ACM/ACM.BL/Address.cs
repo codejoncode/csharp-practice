0fee3991c32e8334c09b26d03f7ff9b8a62479bd
Jonathan <jonathanjholloway@gmail.com>
2019-07-28 19:05:07 -0500

    aded a AddressId and a overload constructor also implemented the Validate method

--- ACM/ACM.BL/Address.cs
+++ ACM/ACM.BL/Address.cs
@@ -12,25 +12,31 @@ namespace ACM.BL
         {
 
         }
-
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+        public int AddressId { get; private set; }
         public string StreetLine1 { get; set; }
         public string StreetLine2 { get; set; }
         public string City { get; set; }
         public string StateProvince { get; set; }
-        public int PostalCode { get; set; }
+        public string PostalCode { get; set; }
         public string Country { get; set; }
-        public string AddressType { get; set; }
+        public int AddressType { get; set; }
 
         ///<summary>
-        ///Validates the address
+        ///Validates the address data
         /// </summary>
         /// <returns>bool</returns>
         /// 
         public bool Validate ()
         {
-            //code for validation  
-            // which parts are required for the user to have updated ? 
-            return true; 
+            var isValid = true;
+
+            if (PostalCode == null) isValid = false; 
+
+            return isValid; 
         }
 
     }
