51760a75d0031e42844151065e2eeccd36ecd3fe
Jonathan <jonathanjholloway@gmail.com>
2019-08-05 16:33:50 -0500

    created a can set name from reference test

--- GradeBook/TypeTests.cs/TypeTests.cs
+++ GradeBook/TypeTests.cs/TypeTests.cs
@@ -7,7 +7,7 @@ namespace TypeTests.cs
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetBookReturnsDifferentObjects()
         {
             //arrange 
             var book1 = GetBook("Book 1");
@@ -28,7 +28,40 @@ namespace TypeTests.cs
         }
 
         [Fact]
-        public void Test2()
+        public void TwoVarsCanReferenceSameObject()
+        {
+            //arrange 
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));//same as Assert.Same
+            //act 
+            //assert 
+
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+            book1.UpdateName("New Name");
+            //act 
+            var expected = "New Name";
+            //assert 
+            Assert.Equal(expected, book1.Name);
+
+        }
+
+        private void SetName(Book book1, string name)
+        {
+            book1.UpdateName(name);
+        }
+
+        [Fact]
+        public void Test1()
         {
             //arrange 
             //act 
