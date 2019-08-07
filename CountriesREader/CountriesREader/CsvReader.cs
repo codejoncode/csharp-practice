1c926610b18d21f5b98b60853cf0b997b1fe042d
Jonathan <jonathanjholloway@gmail.com>
2019-08-07 17:59:03 -0500

    added to the ReadFirstNCountries so that it uses ReadCountryFromCsvLine and brings in StreamReader

--- CountriesREader/CountriesREader/CsvReader.cs
+++ CountriesREader/CountriesREader/CsvReader.cs
@@ -1,4 +1,6 @@
-﻿namespace CountriesREader
+﻿using System.IO;
+
+namespace CountriesREader
 {
     class CsvReader
     {
@@ -11,7 +13,38 @@
 
         public Country[] ReadFirstNCountries(int nCountries)
         {
-            return null;
+            Country[] countries = new Country[nCountries];
+            // can read text files 
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                //read header line 
+                sr.ReadLine();
+
+                for ( int i = 0; i < nCountries; i++)
+                {
+                    //read the next line
+                    string csvLine = sr.ReadLine();
+                    // Parse the line to turn it into a Country instance
+                    countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+            //public String[] Split(params char[] separator);   params takes parmater and then it will build the arrray for you you
+            //so params char[]  takes in arguments and then makes an array. 
+            //allows us not to have to do this new char[] {','} as an argument. 
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
         }
     }
 }
