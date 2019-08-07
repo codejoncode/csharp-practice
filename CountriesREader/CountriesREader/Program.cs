59c33e59d1ba9b67e6ee7bae09e26f9a6d6e1da3
Jonathan <jonathanjholloway@gmail.com>
2019-08-07 18:12:41 -0500

    now using the formatting on the print out

--- CountriesREader/CountriesREader/Program.cs
+++ CountriesREader/CountriesREader/Program.cs
@@ -14,7 +14,7 @@ namespace CountriesREader
 
             foreach (Country country in countries)
             {
-                Console.WriteLine($"{country.Population}: {country.Name}");
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
             }
         }
     }
