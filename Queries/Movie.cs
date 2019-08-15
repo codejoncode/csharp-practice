a2d1a2d3d581f4958cc5605bc6ff5562f5e70073
Jonathan <jonathanjholloway@gmail.com>
2019-08-15 15:11:54 -0500

    added a query project and movie class

---
+++ Queries/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    class Movie
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+
+        public Movie(string title, double rating, int year)
+        {
+            Title = title;
+            Rating = rating;
+            Year = year; 
+        }
+    }
+}
