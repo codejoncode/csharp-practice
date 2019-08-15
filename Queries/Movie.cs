0a7f68b8c50e86c7c20f34236d880e77451ff24c
Jonathan <jonathanjholloway@gmail.com>
2019-08-15 15:26:43 -0500

    practicing filtering with the query and method syntax on the Movies List

--- Queries/Movie.cs
+++ Queries/Movie.cs
@@ -4,17 +4,17 @@ using System.Text;
 
 namespace Queries
 {
-    class Movie
+    public class Movie
     {
         public int Year { get; set; }
         public string Title { get; set; }
         public double Rating { get; set; }
 
-        public Movie(string title, double rating, int year)
-        {
-            Title = title;
-            Rating = rating;
-            Year = year; 
-        }
+        //public Movie(string title, double rating, int year)
+        //{
+        //    Title = title;
+        //    Rating = rating;
+        //    Year = year; 
+        //}
     }
 }
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Queries
 {
@@ -6,7 +8,32 @@ namespace Queries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var movies = new List<Movie>
+            {
+                new Movie {Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
+                new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 },
+                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 }
+            };
+            //query syntax  
+            var query = from movie in movies
+                        where movie.Year > 2000
+                        select movie;
+            
+            //method syntax
+            //where method  it takes a Func<Movie, bool>   bool is the return  and it takes a Movie
+            var query2 = movies.Where(x => x.Year > 2000);
+
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+            foreach (var movie in query2)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+
         }
     }
 }
