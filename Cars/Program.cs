2686a7c4ae96917b7b556f8c0d6d759b6b452be8
Jonathan <jonathanjholloway@gmail.com>
2019-08-17 13:15:38 -0500

    finished cars

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -10,20 +10,38 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("fuel.csv");
-            foreach (var car in cars)
+
+            var query = cars.OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name); // secondary sort if there is a tie
+
+            query =
+                from car in cars
+                orderby car.Combined descending, car.Name ascending
+                select car; 
+            //don't need ascending as it is the default but showing off the keyword. 
+
+            foreach ( var car in query.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Name} : {car.Combined}");
             }
         }
 
         private static List<Car> ProcessFile(string path)
         {
-            return
-            File.ReadAllLines(path)
-                .Skip(1)//skip the header
-                .Where(line => line.Length > 1)
-                .Select(Car.ParseFromCsv)
-                .ToList();
+            /*Method syntax*/
+            //return
+            //File.ReadAllLines(path)
+            //    .Skip(1)//skip the header
+            //    .Where(line => line.Length > 1)
+            //    .Select(Car.ParseFromCsv)
+            //    .ToList();
+            /*The same as the above below query syntax */
+            var query =
+            from line in File.ReadAllLines(path).Skip(1)
+            where line.Length > 1
+            select Car.ParseFromCsv(line);
+            return query.ToList();
+
         }
 
         
